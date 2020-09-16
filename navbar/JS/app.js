5f4daaa418aaa2845c4ca750175c99c8335d6fce
Md Ariful Islam <67740448+smarifuli43@users.noreply.github.com>
2020-09-16 23:06:19 +0600

    change

--- navbar/JS/app.js
+++ navbar/JS/app.js
@@ -1,20 +1,32 @@
-const hamBurger = document.querySelector('.ham');
-const nav = document.querySelector('.links');
-const navLinks = document.querySelectorAll('.links li');
+const hamBurger = document.querySelector(".ham");
+const nav = document.querySelector(".links");
+const navLinks = document.querySelectorAll(".links li");
 
-hamBurger.addEventListener('click', function () {
-    nav.classList.toggle('active');
+hamBurger.addEventListener("click", function () {
+  nav.classList.toggle("active");
 
-    // animation
-    navLinks.forEach(function (link, index) {
-        if (link.style.animation) {
-            link.style.animation = "";
-        } else {
-            link.style.animation = `navFade .4s ease forwards ${index/6+.3}s`;
-        }
-        
+  // animation
+  navLinks.forEach(function (link, index) {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navFade .4s ease  forwards  ${index / 6 + 0.2}s`;
+    }
+  });
+  //  ham burger toggle
+  hamBurger.classList.toggle("toggle");
+});
+
+navLinks.forEach(function (item) {
+  item.addEventListener("click", function () {
+    nav.classList.remove("active");
+    hamBurger.classList.remove("toggle");
+
+    navLinks.forEach(function(link){
+     if(link.style.animation){
+         link.style.animation="";
+     }
     });
-//  ham burger toggle
-   hamBurger.classList.toggle('toggle');
 
+  });
 });
