135a6da33000a1aa11bc10aeccffebe014b7ff2f
Md Ariful Islam <67740448+smarifuli43@users.noreply.github.com>
2020-09-17 17:21:10 +0600

    change3

--- navbar/JS/app.js
+++ navbar/JS/app.js
@@ -22,11 +22,56 @@ navLinks.forEach(function (item) {
     nav.classList.remove("active");
     hamBurger.classList.remove("toggle");
 
-    navLinks.forEach(function(link){
-     if(link.style.animation){
-         link.style.animation="";
-     }
+    navLinks.forEach(function (link) {
+      if (link.style.animation) {
+        link.style.animation = "";
+      }
     });
+  });
+});
 
+const navF = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+
+  const navHeight = navF.getBoundingClientRect().height;
+
+  if (scrollHeight > navHeight) {
+    navF.classList.add("fixed-nav");
+  } else {
+    navF.classList.remove("fixed-nav");
+  }
+});
+
+// scroll
+const scrollLinks = document.querySelectorAll(".scroll");
+
+scrollLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navF.getBoundingClientRect().height;
+    const containerHeight = nav.getBoundingClientRect().height;
+
+    const fixedNav = navF.classList.contains("fixed-nav");
+
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    // if (navHeight > 80) {
+    //   position = position + containerHeight;
+    // }
+  
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
   });
 });
